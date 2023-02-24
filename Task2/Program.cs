71aa35307a51f35e42b6ca95bb0482caf8929cae
Armen <aapapikyan@gmail.com>
2023-02-24 18:28:59 +0300

    Подготовил задачи к семинару 7

---
+++ Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+//  Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1, 7 -> такого числа в массиве нет
+// 1, 2 -> 4
+
+int[,] CreateMatrix()
+{
+    int[,] matrix = new int[3, 4];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return (matrix);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int InputInt(string message)
+{
+    Console.Write($"{message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void CheckPrint(int[,] matrix, int row, int col)
+{
+    if(row - 1 <= matrix.GetLength(0) && col - 1 <= matrix.GetLength(1))
+    {
+        Console.WriteLine($"Значение искомого элемента равно {matrix[row - 1, col - 1]}");
+    }
+    else
+    {
+        Console.WriteLine($"Такого элемента нет в массиве");        
+    }
+}
+int[,] matrix = CreateMatrix();
+PrintArray(matrix);
+int row = InputInt("Введите номе строки > ");
+int col = InputInt("Введите номе столбца > ");
+CheckPrint(matrix, row, col);
---
+++ Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 3: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrix()
+{
+    int[,] matrix = new int[3, 4];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return (matrix);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double AvarageElementsCol(int[,] array, int col)
+{
+    double sum = 0;
+    int i = 0;
+    for(i = 0; i < array.GetLength(0); i++)
+    {
+        sum = sum + array[i, col];
+    }
+    return sum / i;
+}
+
+int[,] matrix = CreateMatrix();
+PrintArray(matrix);
+for(int j = 0; j < matrix.GetLength(1); j++)
+{
+    Console.WriteLine($"Среднее арифметическое элементов столбаца № {j+1} равно {AvarageElementsCol(matrix, j)}");
+}
